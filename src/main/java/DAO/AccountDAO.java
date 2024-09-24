8b97ba8df81cafdb384dbea76024260d1839930d
Andrew Zhang <andrewzhang1600@gmail.com>
2024-09-24 18:50:17 +0000

    Commit_3

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -11,7 +11,7 @@ public class AccountDAO{
 
     public Account registerNewAccount(Account acct){
         Connection conn = ConnectionUtil.getConnection();
-        if (checkAccountExists==false){
+        if (checkAccountExists(acct)==false){
             try {
                 String sql = "INSERT INTO account (username,password) VALUES (?,?)";
                 PreparedStatement ps = conn.prepareStatement(sql);
@@ -21,7 +21,7 @@ public class AccountDAO{
                 ResultSet pkeys = ps.getGeneratedKeys();
                 if(pkeys.next()){
                     int generated_account_id = (int) pkeys.getInt(1);
-                    return new Account(generated_account_id,acct.getUsername(),acct.getPassword())
+                    return new Account(generated_account_id,acct.getUsername(),acct.getPassword());
                 }
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
@@ -31,9 +31,9 @@ public class AccountDAO{
     }
 
     public boolean checkAccountExists(Account acct){
-        Connection connection = ConnectionUtil.getConnection();
+        Connection conn = ConnectionUtil.getConnection();
         try{
-            String sql = "SELECT * FROM account WHERE username=? AND password=?"
+            String sql = "SELECT * FROM account WHERE username=? AND password=?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, acct.getUsername());
             ps.setString(2, acct.getPassword());
